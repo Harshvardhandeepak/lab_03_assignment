8bc8659b001f946417c522c963f95d4d2487e8b6
harshvardhandeepak <harshvardhandeepak@gmail.com>
2023-02-01 16:53:15 +0530

    cmt_msg_022

--- lab 3 software pyhton.py
+++ lab 3 software pyhton.py
@@ -1,5 +1,18 @@
-#--------bla bla------
+class shape:
+    pass
+       
+class circle(shape):
+    def m(self):
+        print("The area of circle is: 100")
+        print("The color of circle is: Blue")
+ 
+class rectangle(shape):
+    def m(self):
+        print("The area of rectangle is: 100")
+        print("The color of rectangle is: Red")  
+     
+obj1 = rectangle()
+obj1.m()
 
-
-
-#--------------
+obj2 = circle()
+obj2.m()
